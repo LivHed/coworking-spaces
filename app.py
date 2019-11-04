6e39162ba452e81fec59c88681145659011a87f3
Ubuntu <ubuntu@ip-172-31-94-213.ec2.internal>
2019-11-04 16:30:04 +0000

    Add route to get coworkingspaces collection

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 ## create an instance of flask and assign it to the app variable
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'coworkingSpacesDB'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+mongo = PyMongo(app)
+
+#this will be the default function that will be called when the application is run
 @app.route('/')
-def hello():
-    return "Hello there"
+@app.route('/get_coworkingspaces')
+def get_coworkingspaces():
+    return render_template('home.html', coworkingspaces=mongo.db.coworkingspaces.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
