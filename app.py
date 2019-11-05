ec48741e9620e0739fc79d94fa3ed13e52f27fd3
Ubuntu <ubuntu@ip-172-31-94-213.ec2.internal>
2019-11-05 11:12:04 +0000

    Change comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-## create an instance of flask and assign it to the app variable
+# create an instance of flask and assign it to the app variable
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'coworkingSpacesDB'
@@ -11,7 +11,7 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-#this will be the default function that will be called when the application is run
+# this will be the default function that will be called when the application is run
 @app.route('/')
 @app.route('/get_coworkingspaces')
 def get_coworkingspaces():
@@ -21,3 +21,4 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
