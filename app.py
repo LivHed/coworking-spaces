011c72dc18ee5e07e6ec8ebdd70720c2b971235b
Ubuntu <ubuntu@ip-172-31-94-213.ec2.internal>
2019-11-15 01:57:11 +0000

    Set debug to False

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def contact_page():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
             
